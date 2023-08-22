3f7a07a3ae554d1cd17f61a308409a1196349791
sergio_manica <serginhomanica@hotmail.comm>
2023-08-21 23:06:23 -0300

    Add circlesMainTimeline

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ const circle1 = document.querySelector(".circle1");
 const circle2 = document.querySelector(".circle2");
 const circle3 = document.querySelector(".circle3");
 const initialbackground = document.querySelector(".initialBackground");
+const leftColumn = document.querySelector(".leftColumn");
+const rightColumn = document.querySelector(".rightColumn");
+
+// Create th animantion timeline of rectangle
+
+const rectangleTimeline1 = gsap.timeline();
+
+    rectangleTimeline1
+      .to(leftColumn, {
+        duration: 3,
+        y: '250vh',
+        ease: 'power2.out',
+      })
+      .to(
+        rightColumn,
+        {
+          duration: 3,
+          y: '-250vh',
+          ease: 'power2.out',
+        },
+        '>-3'
+      );
+
 
 // Set initial properties
 gsap.set([circle1, circle2, circle3], {
@@ -13,16 +36,16 @@ gsap.set([circle1, circle2, circle3], {
 });
 
 
-// Create the animation timelines
+// Create the animation timelines of circle
 const timeline1 = gsap.timeline();
 
 const timeline2 = gsap.timeline();
 
 const timeline3 = gsap.timeline();
 
-const mainTimeline = gsap.timeline();
+const circlesMainTimeline = gsap.timeline();
 
-mainTimeline.add(timeline1).add(timeline2).add(timeline3);
+circlesMainTimeline.add(timeline1).add(timeline2).add(timeline3);
 
 timeline3.to(initialbackground, {
   delay: 1,
@@ -64,27 +87,41 @@ timeline1
 
 // Play the animation
 
-mainTimeline.play(-1);
+rectangleTimeline1.pause();
+circlesMainTimeline.pause();
 
-document.addEventListener("click", function () {
-  if (mainTimeline.paused()) {
-    if (mainTimeline.reversed()) {
-      mainTimeline.reverse();
+document.addEventListener('click', function () {
+  if (circlesMainTimeline.paused() ) {
+    if (circlesMainTimeline.reversed()) {
+      circlesMainTimeline.reverse();
     } else {
-      mainTimeline.play();
+      circlesMainTimeline.play();
     }
   } else {
-    mainTimeline.pause();
+    circlesMainTimeline.pause();
   }
 });
 
-document.addEventListener("keydown", function (event) {
+document.addEventListener('keydown', function (event) {
   const keyName = event.key;
-  if (keyName === " ") {
-    if (mainTimeline.reversed()) {
-      mainTimeline.play();
+  if (keyName === '1') {
+    rectangleTimeline1.play();
+  }
+  if (keyName === '2') {
+    circlesMainTimeline.play();
+  }
+  if (keyName === 'w') {
+    if (circlesMainTimeline.reversed()) {
+      circlesMainTimeline.play();
     } else {
-      mainTimeline.reverse();
+      circlesMainTimeline.reverse();
     }
   }
-});
+  if (keyName === 'q') {
+    if (rectangleTimeline1.reversed()) {
+      rectangleTimeline1.play();
+    } else {
+      rectangleTimeline1.reverse();
+    }
+  }
+})
